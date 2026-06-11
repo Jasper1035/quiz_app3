db2612ea13a1e486e9c58776035a9b7ede47609f
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-06-11 18:35:13 +0100

    quiz_app3  practice

---
+++ lib/screen/quiz_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app3/model/questions.dart';
+import 'package:quiz_app3/widget/answer_card.dart';
+
+class QuizScreen extends StatelessWidget {
+  const QuizScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final question = questions[0];
+    return Scaffold(
+      appBar: AppBar(title: Text('Quiz')),
+      body: Padding(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              question.question,
+              style: TextStyle(fontSize: 23),
+              textAlign: TextAlign.center,
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: question.options.length,
+              itemBuilder: (context, index) {
+                // return GestureDetector(onTap: () {}, child: AnswerCard(question: question, isSelected: isSelected, correctAnswerIndex: correctAnswerIndex, selectedAnswerIndex: selectedAnswerIndex, currentIndex: currentIndex),);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
